b2d3f8ffa373f99f402907ce9a957d0befdad016
Max Hua <70117371+BleakBubbles@users.noreply.github.com>
2024-09-08 14:05:39 -0400

    Created title and board components

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import tuq from "../assets/tuq.png"
+
+export default function Title() {
+    return (
+        <div className="row-span-1 flex flex-row text-9xl">
+            <img src={tuq} alt="Tuq" className="size-[8rem]"/>
+            uq-tac-toe
+        </div>
+    )
+}
